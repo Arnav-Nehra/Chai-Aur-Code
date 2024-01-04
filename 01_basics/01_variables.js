5333e71d63d0374b8ee5676c8d994e29e1349dac
Arnav-Nehra <arnavnehra1@gmail.com>
2024-01-05 01:39:26 +0530

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=4532
+let acoountemail="arnavnehra@gmail.com"
+
+var accountcity="jaipur"
+
+accountpassword="12434"
+let accountstate
+
+// accountId=4546
+/* conole.log(accountId); */
+// we cannot change the value of const else it will give error
+
+accountemail="hchc@gmail.com";
+accountcity="banglore";
+accountpassword="12435";
+
+console.table([accountId,accountemail,accountcity,accountpassword,accountstate]);
+/* prefer not to use var because of issue in block scope and functional scope */
