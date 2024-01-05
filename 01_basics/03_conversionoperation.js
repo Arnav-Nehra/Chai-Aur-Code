689339e7fdcf9554cfed961f19922fdd18df437c
Arnav-Nehra <arnavnehra1@gmail.com>
2024-01-05 23:11:34 +0530

    conversionoperation conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -29,10 +29,38 @@ let valueinnumber3=Number(score3)
 console.log(typeof (valueinnumber3))
 console.log(valueinnumber3) */
 
-let score4=1
+/* let score4=1
 
 console.log(typeof score4)
 
 let valueinnumber4=Boolean(score4)
 console.log(typeof(valueinnumber4))
-console.log(valueinnumber4)
+console.log(valueinnumber4) */
+
+/* ******** Operations ******** */
+/* let value=4
+let negvalue=-value
+console.log(negvalue)
+
+let str1="arnav "
+let str2="nehra"
+
+let str3=str1+str2
+console.log(str3)
+console.table([negvalue,str3]) */
+
+/* console.log("1"+2)
+console.log(1+"2")
+
+console.log("1"+2+2)
+console.log(1+2+"2") */
+console.table([("1"+2),(1+"2"),("1"+2+2),(1+2+"2")])
+
+console.table([(true),(+true),(+"")])
+
+let gamecounter=404
+gamecounter++
+console.log(gamecounter)
+
+//difference between prefix and postfix =>mdn wed docs
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
