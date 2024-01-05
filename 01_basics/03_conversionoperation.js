8e227042d53271b0862f2c26c2960dffc9ddca0d
Arnav-Nehra <arnavnehra1@gmail.com>
2024-01-05 12:47:22 +0530

    conversion operation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,38 @@
+/* let score="33abc"
+
+console.log(typeof score)
+//console.log(typeof(score)) // another way to see the datatype of a variable
+
+let valueinnumber= Number(score)
+console.log(typeof (valueinnumber))
+console.log(valueinnumber)
+ */
+
+/* let score1=33
+console.log(typeof score1)
+
+let valueinnumber1=Number(score1)
+console.log(typeof (valueinnumber1))
+console.log(valueinnumber1) */
+
+/* let score2=null
+console.log(typeof score2)
+
+let valueinnumber2=Number(score2)
+console.log(typeof (valueinnumber2))
+console.log(valueinnumber2) */
+
+/* let score3=undefined
+console.log(typeof score3)
+
+let valueinnumber3=Number(score3)
+console.log(typeof (valueinnumber3))
+console.log(valueinnumber3) */
+
+let score4=1
+
+console.log(typeof score4)
+
+let valueinnumber4=Boolean(score4)
+console.log(typeof(valueinnumber4))
+console.log(valueinnumber4)
