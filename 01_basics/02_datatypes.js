bd1619a62972d5b05fe41ea3d714c695cc380eaa
Arnav-Nehra <arnavnehra1@gmail.com>
2024-01-05 10:51:35 +0530

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version 
+
+// alert(3+3) //we are using node js and not browser
+
+console.log(3+
+    3);//code readibility should be high
+
+console.log("Arnav")
+let name ="arnav "
+let age=18
+let isloggedin=false
+
+//number
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+
+//symbol => unique (used in react to find uniqueness)
+//object 
+
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined type
+
+console.log(typeof 5)
