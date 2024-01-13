533901fe316e963e31739ebbc5df7173c5294f09
Arnav-Nehra <arnavnehra1@gmail.com>
2024-01-13 22:16:27 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month='February'
+
+switch(month){
+    case 1:
+        console.log('January');
+        break;
+    case 'February':
+        console.log('February');
+        break;
+    case 3:
+        console.log('march');
+        break;
+    case 4:
+        console.log('april') ;
+        break;       
+    default:
+        console.log('default case match');
+        break;
+}
