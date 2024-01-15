76a5b6c889c0e5324a3525137c56c90c9d6490dc
Arnav-Nehra <arnavnehra1@gmail.com>
2024-01-15 23:08:47 +0530

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let index=1
+while(index<=10){
+    //console.log(`Value of index is ${index}`)
+    index=index + 1
+}
+
+let myArray=['flash','batman','warner','bros','dc']
+let arr=0
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`)
+    arr=arr+1
+}
+
+let score=1
+do {
+    console.log(`the score is ${score}`)
+    score++
+} while (score<=10);
